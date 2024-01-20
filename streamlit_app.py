ef1be17e7888c908a72b7736cac6b73dc33676e9
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-01-20 15:50:50 -0800

    💄 Clean bets

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,9 @@ import streamlit as st
 import requests
 
 conn = requests.get("http://104.248.109.197:8383/v1/predictions")
-all_bets = pd.DataFrame.from_dict(conn.json()).sort_values(by=['date'])
-bets = all_bets.loc[(all_bets["home"] > 0.55) | (all_bets["away"] > 0.55) | (all_bets["draw"] > 0.55)]
+bets = pd.DataFrame.from_dict(conn.json())
 colname = ['home_team', 'home', 'draw', 'away', 'away_team', 'round', 'date', 'league']
 # ----------------- game start --------
-st.dataframe(bets[colname])
+st.dataframe(bets[colname], hide_index=True)
 
 st.markdown("Made with 💖 by [nies.futbol](https://nies.futbol)")
