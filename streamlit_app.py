97b041744a84ff09c950f80d0e354fde9c0ff290
Nepo Rojas • GECI <braulio.rojas@islas.org.mx>
2024-02-01 20:04:32 -0800

    💄Rename columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,10 @@ import requests
 conn = requests.get("http://104.248.109.197:8383/v1/predictions")
 bets = pd.DataFrame.from_dict(conn.json())
 colname = ['home_team', 'home', 'draw', 'away', 'away_team', 'round', 'date', 'league']
+new_names = ["Local", "Gana local", "Empate", "Gana visita", "Visita", "Jornada", "Fecha", "Liga"]
+to_rename = { k:v for (k,v) in zip(colname, new_names)}
 # ----------------- game start --------
-st.dataframe(bets[colname].sort_values(by=['date', 'league']), hide_index=True)
+to_show = bets[colname].sort_values(by=['date', 'league'])
+st.dataframe(to_show.rename(columns=to_rename), hide_index=True)
 
 st.markdown("Made with 💖 by [nies.futbol](https://nies.futbol)")
