7425aff64fc1d199167f663ad8143f8bcfb1145d
Nepo Rojas • GECI <braulio.rojas@islas.org.mx>
2024-02-14 18:59:44 -0800

    ✍🏿 Add description

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,15 @@ new_names = ["Local", "Gana local", "Empate", "Gana visita", "Visita", "Jornada"
 to_rename = {k: v for (k, v) in zip(colname, new_names)}
 # ----------------- game start --------
 to_show = bets[colname].sort_values(by=["date", "league"])
+st.write(
+  """
+  En la figura de abajo podemos ver un ejemplo de las predicciones hechas por el modelo de [nies](https://www.nies.futbol/).
+
+En las columnas 1 y 2 tenemos el nombre de los equipos.
+En la columna 1 tenemos a los equipos locales y en la columna dos tenemos a los equipos visitantes.
+En las columnas 3 tenemos las probabilidades de que gane el local, el empate o la visita.
+  """
+)
 st.dataframe(to_show.rename(columns=to_rename), hide_index=True)
 
 st.markdown("Made with 💖 by [nies.futbol](https://nies.futbol)")
